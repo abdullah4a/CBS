47ba49d69a017f253da0b1224a91cf3f8c46628b
Abdullah Iqbal <testemail4a@gmail.com>
2025-11-18 02:23:25 +0500

    Add Swagger documentation for root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,22 @@ import healthRoute from './health.js';
 
 const router = Router();
 
+// add Swagger documentation for root route
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Root endpoint
+ *     description: Returns a simple Hello World message.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Hello World!
+ */
 router.get('/', (req, res) => {
     res.send('Hello World!');
 });
