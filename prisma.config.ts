5e1472f65e1dbf68d78302381c2cf08b0ca5311d
Abdullah Iqbal <testemail4a@gmail.com>
2025-11-19 15:27:55 +0500

    Add Prisma setup with initial schema, migrations, and configuration files

---
+++ prisma.config.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config'  // ← Add this line to load .env
+
+export default {
+  schema: './prisma/schema',
+};
---
+++ prisma/schema/migrations/20251119102615_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Tenant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "subdomain" TEXT NOT NULL,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tenant_subdomain_key" ON "Tenant"("subdomain");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_tenantId_idx" ON "User"("tenantId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "Tenant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
